783434c501706563124847a45b54389141003295
Angel Miranda <pixeldev1729@gmail.com>
2023-03-21 08:59:37 -0600

    fix(SpecifiedLogicalTokenizer): use right symbol

--- src/main/java/com/pixeldv/truthtables/lexer/SpecifiedLogicalTokenizer.java
+++ src/main/java/com/pixeldv/truthtables/lexer/SpecifiedLogicalTokenizer.java
@@ -14,25 +14,32 @@ public class SpecifiedLogicalTokenizer extends LogicalTokenizer {
     final char val,
     final @NotNull Deque<Token> tokens
   ) {
-    Token lastToken = tokens.peekLast();
+    final var lastToken = tokens.peekLast();
     if (lastToken == null) {
       throw new IllegalStateException("Invalid operation at " + index + ".");
     }
     final var valString = String.valueOf(val);
-    Token token = null;
-    if (OperationResolver.BINARY_OPERATIONS.get(valString) != null) {
+    Token.Type type = null;
+    Character symbol = OperationResolver.BINARY_OPERATIONS.get(valString);
+    if (symbol != null) {
       if (lastToken.type() != Token.Type.RIGHT_PARENTHESIS &&
           lastToken.type() != Token.Type.VAR) {
         throw new IllegalStateException("Invalid binary operation at " + lastToken.index());
       }
-      token = new Token(val, index, Token.Type.BINARY_OPERATOR);
-      token.setResolved(true);
-      tokens.add(token);
-    } else if (OperationResolver.UNARY_OPERATIONS.get(valString) != null) {
-      token = new Token(val, index, Token.Type.UNARY_OPERATOR);
-      token.setResolved(true);
-      tokens.add(token);
+      type = Token.Type.BINARY_OPERATOR;
+    } else {
+      symbol = OperationResolver.UNARY_OPERATIONS.get(valString);
+
+      if (symbol != null) {
+        type = Token.Type.UNARY_OPERATOR;
+      }
     }
+    if (symbol == null) {
+      return null;
+    }
+    final var token = new Token(symbol, index, type);
+    token.setResolved(true);
+    tokens.add(token);
     return token;
   }
 }
