dbf5e3015888357614eac88da07c124149662799
Angel Miranda <pixeldev1729@gmail.com>
2023-03-21 09:48:20 -0600

    feat: implement truth table

--- src/main/java/com/pixeldv/truthtables/Main.java
+++ src/main/java/com/pixeldv/truthtables/Main.java
@@ -1,16 +1,12 @@
 package com.pixeldv.truthtables;
 
-import com.pixeldv.truthtables.lexer.ResolvableLogicalTokenizer;
 import com.pixeldv.truthtables.lexer.SpecifiedLogicalTokenizer;
-import com.pixeldv.truthtables.lexer.Token;
 import com.pixeldv.truthtables.parser.ExpressionParser;
-import com.pixeldv.truthtables.representation.Expression;
-import org.jetbrains.annotations.NotNull;
+import com.pixeldv.truthtables.table.TruthTable;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -21,104 +17,28 @@ public class Main {
      *"([ ]p [ ] q) [ ] (p [ ] [ ]r)" --> funciona
      * (p [ ] q [ ] r) [ ] ([ ]p [ ] [ ]s) --> funciona
      *
+     * "((~p ^ q) v (p v r)) ^ ~(r ^ q)"
+     *
      * (([ ]p [ ] q) [ ] (p [ ] r)) [ ] [ ](r [ ] q) --> funciona
      */
-//    final var input = "(([ ]p [ ] q) [ ] (p [ ] r)) [ ] [ ](r [ ] q)";
-//    final var tokenizer = new SpecifiedLogicalTokenizer();
-//    final var tokens = tokenizer.tokenize("((¬p ∧ q) ∨ (p ∨ r)) ∧ ¬(r ∧ q)");
-    final var resolvableTokenizer = new ResolvableLogicalTokenizer();
-    final var resolvableTokens = resolvableTokenizer.tokenize("([ ]p [ ] q) [ ] (p [ ] [ ]r)");
     final var specifiedTokenizer = new SpecifiedLogicalTokenizer();
-    final var specifiedTokens = specifiedTokenizer.tokenize("(¬p ∧ q) ∨ (p ∨ ¬r)");
-
-    //    List<Character> operations = List.of('∧', '¬');
-    //    List<Character> operations = List.of('¬', '¬', '∧', '∨', '∨', '∧', '¬', '∧');
-//    List<Character> operations = List.of('¬', '∧', '∨', '∨', '∧', '¬', '∧');
-    List<Character> operations = List.of('¬', '∧', '∨', '∨', '¬');
-    //    List<Character> operations = List.of('↔', '→');
-    //        List<Character> operations = List.of('¬', '∧', '∨', '∨', '¬');
-    int index = 0;
-    //
-    for (Token token : resolvableTokens) {
-            System.out.println(token);
-
-      if (token.unresolved()) {
-        token.setToken(operations.get(index++));
-      }
-    }
+    final var specifiedTokens = specifiedTokenizer.tokenize("((p ^ q) v r) ^ (~p < ~s)");
 
     //    OperationResolver operationResolver = new OperationResolver(input, lexer.tokens());
     //    operationResolver.scan(new Scanner(System.in));
 
-    // resolver operadores
-
-    // (~(~p ^ q) v (p v -r)) ^ ~(r ^ q)
-    // (~(~p ^ q) v (p v -r)) ^ (r ^ q)
-    //
-    final var resolvedExpression = ExpressionParser.parse(new LinkedList<>(resolvableTokens));
+    //    final var resolvedExpression = ExpressionParser.parse(new LinkedList<>(resolvableTokens));
     final var specifiedExpression = ExpressionParser.parse(new LinkedList<>(specifiedTokens));
 
-    if (resolvedExpression == null || specifiedExpression == null) {
-      throw new IllegalStateException("Invalid expression");
-    }
+    //    if (resolvedExpression == null || specifiedExpression == null) {
+    //      throw new IllegalStateException("Invalid expression");
+    //    }
 
-    System.out.println("Resolved expression: " + resolvedExpression.readableForm());
-    System.out.println("Specified expression: " +specifiedExpression.readableForm());
-    System.out.println("Equivalent: " + resolvedExpression.equivalent(specifiedExpression));
-
-//    final var variables = tokens.stream()
-//                            .filter(token -> token.type() == Token.Type.VAR)
-//                            .map(Token::token)
-//                            .collect(Collectors.toSet());
-//
-//    List<Map<Character, Expression.Val>> truthValues = generateCombinations(variables);
-
-    /*
-                 a      b       c             d               e          f
-    p | q | r | ~p | ~p ^ q | p v r | (~p ^ q) v (p v r) | (r ^ q) | ~(r ^ q) | ((~p ^ q) v (p v r)) ^ ~(r ^ q) |
-    V | V | V |  F |    F   |   V   |         V          |    V    |     F    |                  F              |
-    V | V | F |  F |    F   |   V   |         V          |    F    |     V    |                  V              |
-    V | F | V |  F |    F   |   V   |         V          |    F    |     V    |                  V              |
-    V | F | F |  F |    F   |   V   |         V          |    F    |     V    |                  V              |
-    F | V | V |  V |    V   |   V   |         V          |    V    |     F    |                  F              |
-    F | V | F |  V |    V   |   F   |         V          |    F    |     V    |                  V              |
-    F | F | V |  V |    F   |   V   |         V          |    F    |     V    |                  V              |
-    F | F | F |  V |    F   |   F   |         F          |    F    |     V    |                  F              |
-     */
-
-    // use this to print the 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' columns
-//    final var namedExpressions = resolvedExpression.extractNamedExpressions();
-//    final var output = new StringBuilder();
-//    System.out.println(output);
-  }
-
-  private static @NotNull List<Map<Character, Expression.Val>> generateCombinations(
-    final @NotNull Collection<Character> prepositions
-  ) {
-    final var size = prepositions.size();
-    final var combinations = (int) Math.pow(2, size);
-    final var list = new ArrayList<Map<Character, Expression.Val>>(combinations - 1);
-    for (int i = 0; i < combinations; i++) {
-      final var truthValues = new HashMap<Character, Expression.Val>();
-      final var binary = Integer.toBinaryString(i);
-      final var binarySize = binary.length();
-      final var binaryArray = binary.toCharArray();
-      final var iterator = prepositions.iterator();
-      for (int j = 0; j < size; j++) {
-        final var character = iterator.next();
-        if (j < size - binarySize) {
-          truthValues.put(character, Expression.Val.V);
-        } else {
-          truthValues.put(
-            character,
-            binaryArray[j - (size - binarySize)] == '1' ?
-            Expression.Val.F :
-            Expression.Val.V);
-        }
-      }
-      list.add(truthValues);
-    }
-    return list;
+    //    System.out.println("Resolved expression: " + resolvedExpression.readableForm());
+    System.out.println("Specified expression: " + specifiedExpression.readableForm());
+    //    System.out.println("Equivalent: " + resolvedExpression.equivalent(specifiedExpression));
+    //
+    System.out.println(new TruthTable(specifiedTokens, specifiedExpression).createTruthTable());
   }
 }
 
---
+++ src/main/java/com/pixeldv/truthtables/table/TruthTable.java
@@ -0,0 +1,111 @@
+package com.pixeldv.truthtables.table;
+
+import com.pixeldv.truthtables.lexer.Token;
+import com.pixeldv.truthtables.representation.Expression;
+import com.pixeldv.truthtables.representation.NameOperation;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TruthTable {
+  private final List<Character> prepositions;
+  private final Expression expression;
+
+  public TruthTable(final @NotNull Deque<Token> tokens, final @NotNull Expression expression) {
+    this.prepositions = new ArrayList<>();
+    for (final var token : tokens) {
+      if (token.type() == Token.Type.VAR) {
+        final var tokenChar = token.token();
+        if (prepositions.contains(tokenChar)) {
+          continue;
+        }
+        prepositions.add(tokenChar);
+      }
+    }
+    this.expression = expression;
+  }
+
+  public @NotNull String createTruthTable() {
+    final var builder = new StringBuilder();
+    final var prepositionsSize = this.prepositions.size();
+    for (int i = 0; i < prepositionsSize; i++) {
+      builder.append(this.prepositions.get(i));
+      if (i != prepositionsSize - 1) {
+        builder.append(" | ");
+      }
+    }
+    final var subExpressions = this.expression.extractNamedExpressions();
+    for (final var subExpression : subExpressions) {
+      builder.append(" | ")
+        .append(subExpression.expression()
+                  .readableForm());
+    }
+    final var expressionReadableForm = this.expression.readableForm();
+    final var expressionReadableFormLength = expressionReadableForm.length() + 1;
+    builder.append(" | ")
+      .append(expressionReadableForm).append(" |")
+      .append(System.lineSeparator());
+    final var truthValues = this.generateCombinations();
+    for (final Map<Character, Expression.Val> truthValue : truthValues) {
+      for (int j = 0; j < prepositionsSize; j++) {
+        final var character = this.prepositions.get(j);
+        builder.append(truthValue.get(character)
+                         .toString());
+        if (j != prepositionsSize - 1) {
+          builder.append(" | ");
+        }
+      }
+      for (final NameOperation subExpression : subExpressions) {
+        final var readableForm = subExpression.expression()
+                                   .readableForm();
+        final var readableFormLength = readableForm.length() + 1;
+        builder.append(" |");
+        if (readableFormLength % 2 == 0) {
+          builder.append(" ".repeat(readableFormLength / 2));
+        } else {
+          builder.append(" ".repeat(readableFormLength / 2 + 1));
+        }
+        builder.append(subExpression.eval(truthValue))
+          .append(" ".repeat(readableFormLength / 2 - 1));
+      }
+      builder.append(" | ")
+        .append(" ".repeat(expressionReadableFormLength / 2))
+        .append(this.expression.eval(truthValue))
+        .append(" ".repeat(expressionReadableFormLength / 2 - 1))
+        .append("|")
+        .append(System.lineSeparator());
+    }
+    return builder.toString();
+  }
+
+  public @NotNull List<Map<Character, Expression.Val>> generateCombinations() {
+    final var size = this.prepositions.size();
+    final var combinations = (int) Math.pow(2, size);
+    final var list = new ArrayList<Map<Character, Expression.Val>>(combinations - 1);
+    for (int i = 0; i < combinations; i++) {
+      final var truthValues = new HashMap<Character, Expression.Val>();
+      final var binary = Integer.toBinaryString(i);
+      final var binarySize = binary.length();
+      final var binaryArray = binary.toCharArray();
+      final var iterator = this.prepositions.iterator();
+      for (int j = 0; j < size; j++) {
+        final var character = iterator.next();
+        if (j < size - binarySize) {
+          truthValues.put(character, Expression.Val.V);
+        } else {
+          truthValues.put(
+            character,
+            binaryArray[j - (size - binarySize)] == '1' ?
+            Expression.Val.F :
+            Expression.Val.V);
+        }
+      }
+      list.add(truthValues);
+    }
+    return list;
+  }
+}
