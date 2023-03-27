a904d7471e93f5b413f2d770b46fc2aef58c82a1
Angel Miranda <pixeldev1729@gmail.com>
2023-03-27 10:54:54 -0600

    build: set Inferences as main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ tasks {
 
   jar {
     manifest {
-      attributes["Main-Class"] = "com.pixeldv.truthtables.Bootstrap"
+      attributes["Main-Class"] = "com.pixeldv.truthtables.Inferences"
     }
   }
 }
